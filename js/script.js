a800180717462b66a3e48866880bb0f1d81ef3c9
TMateus23 <tomas.g.mateus@gmail.com>
2025-04-23 18:00:24 +0100

    New start

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var $j = jQuery.noConflict(),
+    $w = $j(window);
+
+$j(document).ready(function() {
+    $j('header #block-header .block-header-right .block-burguer-menu').on('click', function() {
+        $j('header #block-header .block-header-middle').addClass('showMenu');
+    });
+
+    $j('header #block-header .block-header-middle .main-menu .button-close-menu').on('click', function (){
+        $j('header #block-header .block-header-middle').removeClass('showMenu');
+    });
+});
+
+$w.on('load', function(){
+
+});
+
+$w.resize(function() {
+
+});
+
+$w.scroll(function() {
+    if($j(this).scrollTop() > 50) {
+        $j('header').addClass('fixed');
+    } else {
+        $j('header').removeClass('fixed');
+    }
+});
